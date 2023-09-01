63d0abeed7a3572d1b12fb9bacf72d3d9c001b17
Piyush Mathpal <mathpal37@gmail.com>
2023-09-01 21:51:32 +0530

    category update

--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,5 @@
 const  express = require("express");
-const {createCategory,getCategory,getAllCategory } =require( "../controllers/category");
+const {createCategory,getCategory,getAllCategory,deleteCategory,updateCategory } =require( "../controllers/category");
 const multer=require("multer")
 const  verifyToken  = require( "../middleware/auth.js");
 const upload = multer({ storage: multer.diskStorage({}),
@@ -13,6 +13,10 @@ router.post("/", verifyToken,upload.single("filepath"),createCategory);
 /* GET */
 router.get("/", verifyToken, getAllCategory);
 router.get("/:category_title", verifyToken, getCategory);
+/*Update*/
+router.put("/update/:category_title", verifyToken, getCategory);
+/*Delete*/ 
+router.delete("/delete/:category_title", verifyToken, deleteCategory)
 
 
 module.exports= router;
