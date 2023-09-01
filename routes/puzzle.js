8ba8072e0bd56a3d00958da000ec840c3caefa03
PRT28 <prithvirajtiwari28@gmail.com>
2023-09-01 23:42:10 +0530

    puzzle list

--- routes/puzzle.js
+++ routes/puzzle.js
@@ -13,6 +13,6 @@ const router = express.Router();
 router.post('/add', verifyToken, addPuzzle);
 router.put('/update', verifyToken, updatePuzzle),
 router.post('/checkAnswer', verifyToken, checkAnswer)
-router.post('/getAllPuzzle', verifyToken, getAllPuzzle)
+router.get('/getAllPuzzle', verifyToken, getAllPuzzle)
 
 module.exports = router;
