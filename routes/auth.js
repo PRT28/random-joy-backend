d926d4820685a3a888d8761327c78f0e9e32f0bf
PRT28 <prithvirajtiwari28@gmail.com>
2023-09-08 23:39:05 +0530

    token details send api

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const  express = require("express");
-const { register,login,getAllUser}= require("../controllers/auth.js");
+const { register,login,getAllUser,authDetails}= require("../controllers/auth.js");
 const  verifyToken  = require( "../middleware/auth.js");
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/users",verifyToken, getAllUser);
+router.get("/details", verifyToken, authDetails)
 
 module.exports =  router 
 // export default router;
