24fbf48fa53955ab6369a73ad64f0e828dae4acc
PRT28 <prithvirajtiwari28@gmail.com>
2023-10-07 13:27:15 +0530

    random commitment and statement api

--- routes/commitment.js
+++ routes/commitment.js
@@ -1,5 +1,5 @@
 const  express = require("express");
-const {createCommitmentOrStatement,getAllCommitment,getAllStatement, deleteCommitment, updateCommitment, takeAction}= require("../controllers/commitment.js");
+const {createCommitmentOrStatement,getAllCommitment,getAllStatement, deleteCommitment, updateCommitment, takeAction, randomCommitment, randomStatement}= require("../controllers/commitment.js");
 const  verifyToken  = require( "../middleware/auth.js");
 const router = express.Router();
 router.post("/",verifyToken, createCommitmentOrStatement);
@@ -8,6 +8,8 @@ router.get("/statement",verifyToken, getAllStatement);
 router.delete("/delete/:id",verifyToken, deleteCommitment)
 router.put("/update/:id",verifyToken, updateCommitment)
 router.patch("/complete",verifyToken, takeAction)
+router.get("/random/commitment", verifyToken, randomCommitment);
+router.get("/random/statement", verifyToken, randomStatement);
 
 module.exports =  router 
 // export default router;
